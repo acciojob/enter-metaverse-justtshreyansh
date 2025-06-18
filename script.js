7ca3c78f1d1176f0718d9396f0feeff25cf50920
Shreyansh <149056401+justtshreyansh@users.noreply.github.com>
2025-06-18 19:43:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const btn = document.querySelector("#enterBtn");
+const p = document.querySelector("#status");
+function eventHandler(){
+	const h1 = document.createElement('h1');
+	h1.innerText = p.innerText;
+	h1.id = p.id;
+	p.parentNode.replaceChild(h1,p);
+}
+btn.addEventListener('click',eventHandler);
